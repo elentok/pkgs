5ed82878562d2351c2b0db2c02d5646b35ff0d8c
David Elentok <3david@gmail.com>
2017-09-03 22:21:03 +0300

    Add bookmarklets builder script

--- bookmarklets/aliexpress.js
+++ bookmarklets/aliexpress.js
@@ -1,25 +1,66 @@
-function parseItem(el) {
-  const img = el.querySelector(".product-sets .pic img");
+/* eslint es6: false */
+/* global copy document */
 
-  return {
-    img: img.src.replace("50x50", "200x200")
-  };
-}
+(function() {
+  function parseItem(el) {
+    var img = el.querySelector(".product-sets .pic img");
+    var a = el.querySelector(".product-title a");
 
-function parseOrder(el) {
-  const [id, date] = el.querySelectorAll(".order-head .order-info .info-body");
+    var amountItems = el.querySelectorAll(".product-amount span");
 
-  const items = Array.from(el.querySelectorAll(".order-body")).map(parseItem);
+    return {
+      img: img.src.replace("50x50", "200x200"),
+      title: a.title,
+      id: a.getAttribute("productid"),
+      url: a.href,
+      cost: amountItems[0].innerText.trim(),
+      quantity: parseInt(amountItems[1].innerText.replace("X", ""), 10)
+    };
+  }
 
-  return {
-    id: id.innerText,
-    date: new Date(date.innerText),
-    items
-  };
-}
+  function parseOrder(el) {
+    var elements = el.querySelectorAll(".order-head .order-info .info-body");
+    var id = elements[0];
+    var date = elements[1];
 
-orders = Array.from(document.querySelectorAll(".order-item-wraper")).map(
-  parseOrder
-);
+    var items = Array.from(el.querySelectorAll(".order-body")).map(parseItem);
 
-copy(JSON.stringify(orders));
+    return {
+      id: id.innerText,
+      date: new Date(date.innerText),
+      items: items
+    };
+  }
+
+  function showPopup(text) {
+    var el = document.createElement("div");
+    el.style.position = "fixed";
+    el.style.left = "15%";
+    el.style.right = "15%";
+    el.style.top = "15%";
+    el.style.bottom = "15%";
+
+    var button = document.createElement("button");
+    button.innerText = "Close";
+    button.style.fontSize = "20px";
+    button.style.fontWeight = "bold";
+    button.addEventListener("click", function() {
+      el.remove();
+    });
+    el.appendChild(button);
+
+    var textarea = document.createElement("textarea");
+    textarea.style.width = "100%";
+    textarea.style.height = "90%";
+    textarea.value = text;
+    el.appendChild(textarea);
+
+    document.body.appendChild(el);
+  }
+
+  var orders = (window.orders = Array.from(
+    document.querySelectorAll(".order-item-wraper")
+  ).map(parseOrder));
+
+  showPopup(JSON.stringify(orders, null, 2));
+})();
